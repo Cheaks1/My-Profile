adafb1f2edcbf7d6776338b4142a23eb6df4e04c
brahimi chakib <chakibbrahimi007@gmail.com>
2023-03-26 21:02:56 +0200

    Add Skills Section Part #01

--- project/js/script.js
+++ project/js/script.js
@@ -3,28 +3,29 @@ whiteTheme = document.querySelector(".light");
 themeContainer = document.querySelector(".theme_icons");
 
 //change betwwen dark and white
-whiteTheme.onclick = function () {
-  document.body.className = "light_theme";
-  this.style.display = "none";
-  darkTheme.style.display = "block";
+whiteTheme.onclick = function() {
+    document.body.className = "light_theme";
+    this.style.display = "none";
+    darkTheme.style.display = "block";
 };
-darkTheme.onclick = function () {
-  document.body.className = "dark_theme";
-  this.style.display = "none";
-  whiteTheme.style.display = "block";
+darkTheme.onclick = function() {
+    document.body.className = "dark_theme";
+    this.style.display = "none";
+    whiteTheme.style.display = "block";
 };
 
 // scroll down btn
 let scroller = document.querySelector(".scroller");
-scroller.onclick = function () {
-  window.scrollTo(0, 969);
+scroller.onclick = function() {
+    window.scrollTo(0, 969);
 };
 
 //give all imgs alternate attr
 let allImg = document.querySelectorAll("img");
 console.log(allImg);
-allImg.forEach(function (img) {
-  img.setAttribute("alt", "cheaks portfolio");
+allImg.forEach(function(img) {
+    img.setAttribute("alt", "cheaks portfolio");
+    img.setAttribute("loading", "lazy");
 });
 
 // Init ScrollMagic controller
@@ -34,19 +35,19 @@ let animeArr = [".img_container ", ".text_container"];
 // Create a ScrollMagic scene for the element
 
 var scene = new ScrollMagic.Scene({
-  triggerElement: ".img_container , .text_container ,.navList",
-})
-  .setClassToggle(".img_container , .text_container,.navList", "show")
-  .addTo(controller);
+        triggerElement: ".img_container , .text_container ,.navList",
+    })
+    .setClassToggle(".img_container , .text_container,.navList", "show")
+    .addTo(controller);
 
 //show the nav bar
 let navList = document.querySelector(".navList");
 let aboutSection = document.querySelector(".about");
 
-window.onscroll = function () {
-  if (this.scrollX > 469) {
-    navList.classlist.add("show");
-  } else {
-    navList.classlist.remove("show");
-  }
-};
+// window.onscroll = function() {
+//     if (this.scrollX > 469) {
+//         navList.classlist.add("show");
+//     } else {
+//         navList.className = "";
+//     }
+// };
